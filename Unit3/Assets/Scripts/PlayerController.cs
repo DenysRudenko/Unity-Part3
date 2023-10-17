7cdcefc0774a8adcc0b1eaae2b0bf9c59410cb7f
DenysRudenko <heartbreathing19951014@gmail.com>
2023-10-18 00:03:38 +0100

    Added a sound effects and sound variables.

--- Unit3/Assets/Scripts/PlayerController.cs
+++ Unit3/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,13 @@ public class PlayerController : MonoBehaviour
     public float gravityModifer;
     public bool isOnGround = true;
     public bool gameOver = false;
-
     private Animator playerAnim;
+
+    public ParticleSystem explosionParticle;
+
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +35,7 @@ public class PlayerController : MonoBehaviour
                     playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                     isOnGround = false;
                     playerAnim.SetTrigger("Jump_trig");
+                    dirtParticle.Stop();
             }
         }
     }
@@ -39,11 +45,14 @@ public class PlayerController : MonoBehaviour
         
         if (collision.gameObject.CompareTag("Ground")) {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle")) {
             gameOver = true;
             Debug.Log("Game Over");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }
