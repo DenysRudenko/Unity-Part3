51d99bd1755d495571447ff9df39933e97023538
DenysRudenko <heartbreathing19951014@gmail.com>
2023-10-18 01:02:38 +0100

    Finished the 3rd challenge.

--- Unit3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Unit3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -6,6 +6,8 @@ public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
 
+    public bool isLowEnough;
+
     public float floatForce;
     private float gravityModifier = 1.5f;
     private Rigidbody playerRb;
@@ -16,6 +18,8 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+
+    public AudioClip bounceSound;
 
 
     // Start is called before the first frame update
@@ -32,12 +36,19 @@ public class PlayerControllerX : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
+
+        if (transform.position.y > 13) {
+            isLowEnough = false;
+        } else {
+            isLowEnough = true;
+        }
+        
     }
 
     private void OnCollisionEnter(Collision other)
@@ -59,6 +70,9 @@ public class PlayerControllerX : MonoBehaviour
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
 
+        } else if (other.gameObject.CompareTag("Ground") && !gameOver) {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.5f);
         }
 
     }
